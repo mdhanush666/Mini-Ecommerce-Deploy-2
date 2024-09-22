0345946a30ff5d82c9c51340df6692ab7ccc6548
M.Dhanush <m.dhanush666dhanu@gmail.com>
2024-09-22 22:15:54 +0530

    Delete frontend directory

--- frontend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return <footer className="py-1 bg-dark">
-    <p className="text-center text-white mt-1 ">
-      D Shopping - &copy; 2024, All Rights Reserved
-    </p>
-  </footer>
-}
